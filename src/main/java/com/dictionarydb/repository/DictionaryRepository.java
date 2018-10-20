81d667f30d80fcca1964c843c47d2c32c4382c14
myumyun <muminayyildizz@gmail.com>
2018-10-20 07:22:03 +0300

    continue

--- src/main/java/com/dictionarydb/repository/DictionaryRepository.java
+++ src/main/java/com/dictionarydb/repository/DictionaryRepository.java
@@ -1,14 +1,8 @@
 package com.dictionarydb.repository;
 
-import java.util.List;
-
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dictionarydb.entity.Dictionary;
 
 public interface DictionaryRepository extends JpaRepository<Dictionary, Integer> { 
-	
-	List<Dictionary> findFirst10ByName(String username);
-	List<Dictionary> findFirst(Pageable pageRequest);
 }
