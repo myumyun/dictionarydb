c018591a0c2beb196b2516a1b3c8a86a21a4db77
myumyun <muminayyildizz@gmail.com>
2019-02-03 11:28:25 +0300

    1.1

--- src/main/java/com/dictionarydb/service/impl/ConfigServiceImpl.java
+++ src/main/java/com/dictionarydb/service/impl/ConfigServiceImpl.java
@@ -5,8 +5,6 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
